8108f52bb13153100e9f8bf094674a115d0c320d
xie_yh <540140031@qq.com>
2018-06-28 17:25:28 +0800

    build module

---
+++ hydrofoil-core/src/main/java/org/hydrofoil/core/jsr223/HydrofoilGraphGremlinPlugin.java
@@ -0,0 +1,24 @@
+package org.hydrofoil.core.jsr223;
+
+import org.apache.tinkerpop.gremlin.jsr223.AbstractGremlinPlugin;
+import org.apache.tinkerpop.gremlin.jsr223.DefaultImportCustomizer;
+import org.apache.tinkerpop.gremlin.jsr223.ImportCustomizer;
+
+/**
+ * HydrofoilGraphGremlinPlugin
+ * <p>
+ * package org.hydrofoil.core.jsr223
+ *
+ * @author xie_yh
+ * @date 2018/6/28 17:19
+ */
+public final class HydrofoilGraphGremlinPlugin extends AbstractGremlinPlugin {
+
+    private final static String NAME = "org.hydrofoil";
+
+    private static final ImportCustomizer imports = DefaultImportCustomizer.build().create();
+
+    public HydrofoilGraphGremlinPlugin() {
+        super(NAME, imports);
+    }
+}
