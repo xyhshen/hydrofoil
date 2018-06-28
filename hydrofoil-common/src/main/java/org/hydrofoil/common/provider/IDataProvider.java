3f493cff9aeade6a5b5bddca7201fab9ab4f79b4
xie_yh <540140031@qq.com>
2018-06-28 17:23:03 +0800

    build module

---
+++ hydrofoil-common/src/main/java/org/hydrofoil/common/provider/IDataProvider.java
@@ -0,0 +1,12 @@
+package org.hydrofoil.common.provider;
+
+/**
+ * IDataProvider
+ * <p>
+ * package org.hydrofoil.common.provider
+ *
+ * @author xie_yh
+ * @date 2018/6/28 17:11
+ */
+public interface IDataProvider {
+}
